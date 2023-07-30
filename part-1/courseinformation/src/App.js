9629ab66d4e5c4194f981c6cf408ccff79937885
Tushar Luitel <tusharluitel88@gmail.com>
2023-07-30 19:18:44 +0545

    Chore: Completed exercise 1.5

--- part-1/courseinformation/src/App.js
+++ part-1/courseinformation/src/App.js
@@ -1,3 +1,5 @@
+import React from "react";
+
 const Header = ({ course }) => {
   return (
     <>
@@ -8,9 +10,9 @@ const Header = ({ course }) => {
 
 const Part = ({ part, exercises }) => {
   return (
-    <>
+    <p>
       {part} {exercises}
-    </>
+    </p>
   );
 };
 
@@ -19,40 +21,35 @@ const Content = ({ parts }) => {
     <div>
       {parts.map((part) => {
         return (
-          <div>
-            <Part key={part.name} part={part.name} exercises={part.exercises} />
-          </div>
+          <React.Fragment key={part.name}>
+            <Part part={part.name} exercises={part.exercises} />
+          </React.Fragment>
         );
       })}
     </div>
   );
 };
 
-const Total = ({ total }) => {
-  return (
-    <>
-      <p>Number of exercises: {total}</p>
-    </>
-  );
-};
-
 const App = () => {
   const course = "Half Stack application development";
   const parts = [
-    { name: "Fundamentals of React", exercises: 10 },
-    { name: "Using props to pass data", exercises: 7 },
-    { name: "State of a component", exercises: 14 },
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
   ];
-
-  const exercises1 = 10;
-  const exercises2 = 7;
-  const exercises3 = 14;
-
   return (
     <div>
       <Header course={course} />
       <Content parts={parts} />
-      <Total total={exercises1 + exercises2 + exercises3} />
     </div>
   );
 };
